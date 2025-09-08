42ec47fea584ee9ba75281c220f694de555e3bf3
aniket-thakoor <aniket.thakoor10@gmail.com>
2025-09-08 16:24:18 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pathology-lab/sw.js', { scope: '/pathology-lab/' })})}
